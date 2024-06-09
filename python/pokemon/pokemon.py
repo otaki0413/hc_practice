a8c8c928e9b766fdd98a368b4bf01a0b85b4ec14
otaki0413 <takida0413@gmail.com>
2024-06-09 16:57:05 +0900

    ✨ 名前修飾により、プライペート変数の似た機能をつくる

--- python/pokemon/pokemon.py
+++ python/pokemon/pokemon.py
@@ -3,7 +3,7 @@ import abc
 
 class Pokemon(metaclass=abc.ABCMeta):
     def __init__(self, name, type1, type2, hp):
-        self.name = name
+        self.__name = name  # __で名前修飾する
         self.type1 = type1
         self.type2 = type2
         self.hp = hp
@@ -12,22 +12,35 @@ class Pokemon(metaclass=abc.ABCMeta):
     def attack(self):
         pass
 
+    def changeName(self, new_name):
+        # 不適切な名前はエラー
+        if new_name == "うんこ":
+            print("不適切な名前です")
+            return
+        self.__name = new_name
+
+    def getName(self):
+        print(self.__name)
+
 
 class Pikachu(Pokemon):
     def attack(self):
-        print(f"{self.name} の10万ボルト!")
+        # 名前修飾によりself.__nameでアクセスできなくなるため、super().getName()からアクセス
+        print(f"{super().getName()} の10万ボルト!")
 
 
 class Zenigame(Pokemon):
     def attack(self):
-        print(f"{self.name} のみずでっぽう!")
+        print(f"{super().getName()} のみずでっぽう!")
 
 
 def main():
     pika = Pikachu("ピカチュウ", "でんき", "", 100)
-    zeni = Zenigame("ゼニガメ", "みず", "", 120)
     pika.attack()
-    zeni.attack()
+    pika.changeName("テキセツ")
+    pika.getName()
+    pika.changeName("うんこ")
+    pika.getName()
 
 
 if __name__ == "__main__":
