2b483749b45fc89b180637edb295dcbc7d21c99e
otaki0413 <takida0413@gmail.com>
2024-06-09 10:51:23 +0900

    ✨ Pokemonクラスの継承できるようにした

--- python/pokemon/pokemon.py
+++ python/pokemon/pokemon.py
@@ -1,21 +1,31 @@
 class Pokemon:
-    def __init__(self, name, type1, type2, hp, mp):
+    def __init__(self, name, type1, type2, hp):
         self.name = name
         self.type1 = type1
         self.type2 = type2
         self.hp = hp
-        self.mp = mp
 
     def attack(self):
         print(f"{self.name} のこうげき!")
 
 
+class Pikachu(Pokemon):
+    def attack(self):
+        super().attack()
+        print(f"{self.name} の10万ボルト!")
+
+
+class Zenigame(Pokemon):
+    def attack(self):
+        super().attack()
+        print(f"{self.name} のみずでっぽう!")
+
+
 def main():
-    poke: Pokemon = Pokemon()
-    print(poke.name)
-    print(poke.type1)
-    print(poke.mp)
-    poke.attack()
+    pika = Pikachu("ピカチュウ", "でんき", "", 100)
+    zeni = Zenigame("ゼニガメ", "みず", "", 120)
+    pika.attack()
+    zeni.attack()
 
 
 if __name__ == "__main__":
