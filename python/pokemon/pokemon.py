5794729e7b7807f547279d60930fc6a1da7a1c37
otaki0413 <takida0413@gmail.com>
2024-06-09 17:50:36 +0900

    ✨ インターフェースとなるNameServiceクラスの実装

--- python/pokemon/pokemon.py
+++ python/pokemon/pokemon.py
@@ -1,7 +1,17 @@
 import abc
 
 
-class Pokemon(metaclass=abc.ABCMeta):
+class NameService(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def changeName(self, new_name):
+        pass
+
+    @abc.abstractmethod
+    def getName(self):
+        pass
+
+
+class Pokemon(NameService, metaclass=abc.ABCMeta):
     def __init__(self, name, type1, type2, hp):
         self.__name = name  # __で名前修飾する
         self.type1 = type1
@@ -13,34 +23,52 @@ class Pokemon(metaclass=abc.ABCMeta):
         pass
 
     def changeName(self, new_name):
-        # 不適切な名前はエラー
         if new_name == "うんこ":
             print("不適切な名前です")
             return
         self.__name = new_name
 
     def getName(self):
-        print(self.__name)
+        return self.__name
+
+
+class Player(NameService, metaclass=abc.ABCMeta):
+    def __init__(self, name):
+        self.__name = name
+
+    def changeName(self, new_name):
+        if new_name == "うんこ":
+            print("不適切な名前です")
+            return
+        self.__name = new_name
+
+    def getName(self):
+        return self.__name
 
 
 class Pikachu(Pokemon):
     def attack(self):
-        # 名前修飾によりself.__nameでアクセスできなくなるため、super().getName()からアクセス
-        print(f"{super().getName()} の10万ボルト!")
+        print(f"{self.getName()} の10万ボルト!")
 
 
 class Zenigame(Pokemon):
     def attack(self):
-        print(f"{super().getName()} のみずでっぽう!")
+        print(f"{self.getName()} のみずでっぽう!")
 
 
 def main():
     pika = Pikachu("ピカチュウ", "でんき", "", 100)
     pika.attack()
     pika.changeName("テキセツ")
-    pika.getName()
+    print(pika.getName())
     pika.changeName("うんこ")
-    pika.getName()
+    print(pika.getName())
+
+    kei = Player("けい")
+    kei.changeName("けい2")
+    print(kei.getName())
+    kei.changeName("うんこ")
+    print(kei.getName())
 
 
 if __name__ == "__main__":
