e69d8e19a8508cf6aba41474433f719e425bdc41
otaki0413 <takida0413@gmail.com>
2024-06-16 15:40:03 +0900

    ✨ Juiceクラスのインスタンス変数をプライベートにして、メソッドからアクセスできるようにした

--- python/vending_machine/juice.py
+++ python/vending_machine/juice.py
@@ -1,9 +1,17 @@
 class Juice:
     def __init__(self, name, price):
-        self.name = name
-        self.price = price
+        self.__name = name
+        self.__price = price
+
+    def get_name(self):
+        """ドリンク名を取得する"""
+        return self.__name
+
+    def get_price(self):
+        """ドリンクの値段を取得する"""
+        return self.__price
 
 
 if __name__ == "__main__":
     pepci = Juice(name="ペプシ", price=150)
-    print(pepci.price)
+    print(pepci.get_price())
